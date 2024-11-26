13315b262575242c7fdd2c65ddbf5870c4e886f2
HaveYouTriedDuckTape <HaveYouTriedDuckTape@gmail.com>
2024-11-26 18:42:09 +0100

    Done for the day -> https://www.youtube.com/live/tjDgH1C_S1Y

--- Streamer bot Extension CSharp/CPHInline.cs
+++ Streamer bot Extension CSharp/CPHInline.cs
@@ -1,44 +1,58 @@
-﻿namespace Streamer_bot_Extension_CSharp;
+﻿// using System;
+using System.Text.RegularExpressions;
 
-using System;
-
+//namespace Streamer_bot_Extension_CSharp;
 public class CPHInline
 {
     public bool Execute()
     {
-        CPH.TryGetArg<string>("rawInput",out string rawInput);
-        CPH.TryGetArg<string>("reply.threadUserLogin",out string userNareply_threadUserLoginme);
-        CPH.TryGetArg<string>("userName",out string userName);
-        CPH.TryGetArg<string>("input0",out string input0);
-        CPH.TryGetArg<string>("triggerName",out string triggerName);
+        CPH.TryGetArg<string>("rawInput", out string rawInput);
+        CPH.TryGetArg<string>("reply.threadUserLogin", out string replythreadUserLogin);
+        CPH.TryGetArg<string>("userName", out string userName);
+        CPH.TryGetArg<string>("input0", out string input0);
+        CPH.TryGetArg<string>("triggerName", out string triggerName);
+        
         string ChatGPT_SkippedMessages = CPH.GetGlobalVar<string>("ChatGPT_SkippedMessages");
         
-        if (!int.TryParse(Console.ReadLine(), out int age) || age < 18)
+        Match regexChatcommandCheck = Regex.Match(rawInput, "^![^\\s]+");
+        if (regexChatcommandCheck.Success)
         {
-            Console.WriteLine("Zugriff verweigert. Sie müssen mindestens 18 Jahre alt sein.");
-            return;
+            CPH.LogDebug($"regexChatcommandCheck: '{rawInput}' was a chat command");
+            return false;
         }
         
+        if (!string.IsNullOrEmpty(replythreadUserLogin))
+        {
+            Match isducktapeBotInThreadCheck = Regex.Match(replythreadUserLogin, "ducktapeBot");
+            if (isducktapeBotInThreadCheck.Success)
+            {
+                CPH.LogDebug($"isducktapeBotInThreadCheck: '{rawInput}' ducktapeBot is in Thread");
+                return false;
+            }
+        }
+        else
+        {
+            CPH.LogDebug($"isducktapeBotInThreadCheck: '{rawInput}' is null or empty");
+        }
+
         return true;
     }
+
     public bool Shoutout()
     {
         // your main code goes here
-
         return true;
     }
+
     public bool ResponseToRandomChat()
     {
         // your main code goes here
-
         return true;
     }
+
     public bool AnsweringQuestions()
     {
         // your main code goes here
-
         return true;
     }
-
-    
 }
